357df1d9c39294b8712e50819ee0d7f5fa418a5f
Selahattin Yunus Yavuz <yunusssyavuzzz@hotmail.com>
2021-08-19 02:13:11 +0300

    19/08/2021 Ders sonu.

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Yunus");
+            Console.WriteLine(isimler.Length);
+            isimler.Add("Gani");
+            Console.WriteLine(isimler.Length);
+
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+        }
+    }
+}
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] {"Yunus", "Akile","Yavuz" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+
+            //isimler = new string[5];
+            //isimler[4] = "Gani";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+            List<string> isimler2 = new List<string>() { "Yunus", "Akile", "Yavuz" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            isimler2.Add("Gani");
+            Console.WriteLine(isimler2[3]);
+            Console.WriteLine(isimler2[0]);
+        }
+    }
+}
