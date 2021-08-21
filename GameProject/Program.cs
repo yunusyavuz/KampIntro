d7a7a4a1494484c86c1ca262bda1f7206eb61470
Selahattin Yunus Yavuz <yunusssyavuzzz@hotmail.com>
2021-08-22 00:38:11 +0300

    22/08/2021 Ders sonu.

---
+++ GameProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //  GamerManager gamerManager= new GamerManager(new UserValidationManager());
+            GamerManager gamerManager = new GamerManager(new NewEStateUserValidationManager());
+            gamerManager.Add(new Gamer() 
+            { Id=1, BirthYear= 1987, FirstName="Yunus",
+             LastName= "Yavuz", IdentityNumber = 12345});
+
+        }
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+﻿Select * from Customers
+select COUNT(*) Adet from Products where CategoryID = 2
+-- Hangi kategoride kac farklı urun var
+select CategoryID, COUNT(*) from Products where UnitPrice>20 group by CategoryID having COUNT(*) <10
+
+select * 
+from Products inner join Categories 
+on Products.CategoryID = Categories.CategoryID
+
+select Products.ProductID, Products.ProductName, Products.UnitPrice, Categories.CategoryName 
+from Products inner join Categories 
+on Products.CategoryID = Categories.CategoryID
+where Products.UnitPrice >10
+
+select * from Products p inner join [Order Details] od
+on p.ProductID = od.ProductID
+
+
